c050b2187a73ce2fc23584b9b8ccd35fce452cbf
Shaun Niel Ochavo <shaunniel02@gmail.com>
2023-10-11 17:22:51 +0800

    Create UI for chat page

---
+++ src/app/components/Conversation.js
@@ -0,0 +1,123 @@
+import { BsArrowReturnRight } from "react-icons/bs";
+import { IoExitOutline } from "react-icons/io5";
+import { AiFillCloseCircle } from "react-icons/ai";
+import { useEffect, useState } from "react";
+
+const SentMessage = ({ msg }) => {
+  return (
+    <div className="rounded-full self-end py-1 px-3 text-white bg-[#8C52FF]">
+      <p>{msg}</p>
+    </div>
+  );
+};
+
+const ReceiveMessage = ({ msg }) => {
+  return (
+    <div className="rounded-full self-start py-1 px-3 text-white bg-gray-500">
+      <p>{msg}</p>
+    </div>
+  );
+};
+
+const Conversation = ({ setIsMatchFound }) => {
+  useEffect(() => {
+    const handleBeforeUnload = (event) => {
+      // Perform actions before the component unloads
+      event.preventDefault();
+      event.returnValue = "";
+    };
+    window.addEventListener("beforeunload", handleBeforeUnload);
+    return () => {
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+    };
+  }, []);
+  const handleEndConvo = () => {
+    let exit = confirm("Are you sure you want to end this conversation?");
+
+    exit && setIsMatchFound(false);
+  };
+
+  return (
+    <div className="w-screen h-[100dvh] flex flex-col items-center">
+      <div className="w-full py-4 px-3 border-b-2 shadow-2xl flex items-center justify-between">
+        <IoExitOutline color="white" size={25} />
+
+        <div className="flex flex-col items-center">
+          <p className="text-xs text-gray-400">You're chatting with</p>
+          <p className="text-md font-medium">shaunniekins123</p>
+        </div>
+        <button onClick={handleEndConvo}>
+          <IoExitOutline color="purple" size={25} />
+        </button>
+      </div>
+
+      <div className="convo-area bg-gray-100 w-full h-full px-3 pb-5 flex flex-col-reverse justify-start space-y-2 overflow-y-auto">
+        <SentMessage msg={"1"} />
+        <SentMessage msg={"hi"} />
+        <ReceiveMessage msg={"wassup"} />
+        <SentMessage msg={"hello"} />
+        <SentMessage msg={"hi"} />
+        <ReceiveMessage msg={"wassup"} />
+        <SentMessage msg={"hello"} />
+        <SentMessage msg={"hi"} />
+        <ReceiveMessage msg={"wassup"} />
+        <SentMessage msg={"hello"} />
+        <SentMessage msg={"hi"} />
+        <ReceiveMessage msg={"wassup"} />
+        <SentMessage msg={"hello"} />
+        <SentMessage msg={"hi"} />
+        <ReceiveMessage msg={"wassup"} />
+        <SentMessage msg={"hello"} />
+        <SentMessage msg={"hi"} />
+        <ReceiveMessage msg={"wassup"} />
+        <SentMessage msg={"hello"} />
+        <SentMessage msg={"hi"} />
+        <ReceiveMessage msg={"wassup"} />
+        <SentMessage msg={"hello"} />
+        <SentMessage msg={"hi"} />
+        <ReceiveMessage msg={"wassup"} />
+        <SentMessage msg={"hello"} />
+        <SentMessage msg={"hi"} />
+        <ReceiveMessage msg={"wassup"} />
+        <SentMessage msg={"hello"} />
+        <SentMessage msg={"hi"} />
+        <ReceiveMessage msg={"wassup"} />
+        <SentMessage msg={"hello"} />
+        <SentMessage msg={"hi"} />
+        <ReceiveMessage msg={"wassup"} />
+        <SentMessage msg={"hello"} />
+        <SentMessage msg={"hi"} />
+        <ReceiveMessage msg={"wassup"} />
+        <SentMessage msg={"hello"} />
+        <SentMessage msg={"hi"} />
+        <ReceiveMessage msg={"wassup"} />
+        <SentMessage msg={"hello"} />
+        <SentMessage msg={"hi"} />
+        <ReceiveMessage msg={"wassup"} />
+        <SentMessage msg={"hello"} />
+        <SentMessage msg={"hi"} />
+        <ReceiveMessage msg={"wassup"} />
+        <SentMessage msg={"hello"} />
+        <SentMessage msg={"hi"} />
+        <ReceiveMessage msg={"wassup"} />
+        <SentMessage msg={"hello"} />
+        <SentMessage msg={"hi"} />
+        <ReceiveMessage msg={"wassup"} />
+      </div>
+
+      <div className="chat w-full py-3 px-3 flex space-x-3 bg-white">
+        <textarea
+          name="message"
+          id="message"
+          rows="1"
+          placeholder="Type Message"
+          className="w-full border-none outline-none rounded-full pl-3 py-2 bg-gray-200 flex items-center"></textarea>
+        <button className="rounded-full p-3 bg-[#8C52FF]">
+          <BsArrowReturnRight color="white" />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Conversation;
--- src/app/components/Main.js
+++ src/app/components/Main.js
@@ -3,8 +3,10 @@
 import { useState, useEffect } from "react";
 
 import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
+import { BsArrowReturnRight } from "react-icons/bs";
 import { TbHeartHandshake } from "react-icons/tb";
 import Navbar from "./Navbar";
+import Conversation from "./Conversation";
 
 const SearchingText = () => {
   const [dots, setDots] = useState("");
@@ -32,42 +34,51 @@ const Main = () => {
   //colors:
   // 5E17EB (dark), 8C52FF (light)
   const [isSearchActivated, setSearchActivated] = useState(false);
-  const [matchFound, setMatchFound] = useState(false);
+  const [isMatchFound, setIsMatchFound] = useState(true);
 
   return (
-    <div className="w-screen h-[100dvh] flex flex-col items-center font-Roboto overflow-hidden">
-      {!isSearchActivated ? (
+    <div className="w-screen h-[100dvh] flex flex-col items-center font-Roboto overflow-hidden ">
+      {!isSearchActivated && !isMatchFound ? (
         <Navbar />
       ) : (
-        <button
-          className="self-start m-5 text-xs text-gray-400 rounded-full z-30"
-          onClick={() => setSearchActivated(false)}>
-          Cancel
-        </button>
+        isSearchActivated &&
+        !isMatchFound && (
+          <button
+            className="self-start m-5 text-xs text-gray-400 rounded-full z-30"
+            onClick={() => setSearchActivated(false)}>
+            Cancel
+          </button>
+        )
       )}
-      <div className="w-full h-full mt-[-100px] flex items-center justify-center relative">
-        {isSearchActivated ? (
-          <>
-            <div className="circle2 pulseCircle2 w-[600px] h-[600px] z-0 absolute bg-purple-400 rounded-full" />
-            <div className="circle1 pulseCircle1 w-[400px] h-[400px] z-10 absolute bg-purple-300 rounded-full" />
-          </>
-        ) : null}
+      {!isMatchFound ? (
+        <div className="w-full h-full flex flex-col items-center">
+          <div className="w-full h-full mt-[-100px] flex items-center justify-center relative">
+            {isSearchActivated ? (
+              <>
+                <div className="circle2 pulseCircle2 w-[600px] h-[600px] z-0 absolute bg-purple-400 rounded-full" />
+                <div className="circle1 pulseCircle1 w-[400px] h-[400px] z-10 absolute bg-purple-300 rounded-full" />
+              </>
+            ) : null}
 
-        <button
-          className={`heart bg-[#8C52FF] rounded-full p-5 ${
-            isSearchActivated ? "pulse morph-active" : "morph"
-          } relative z-20`}
-          onClick={() => setSearchActivated(true)}>
-          <AiFillHeart size={isSearchActivated ? 185 : 130} color="white" />
-        </button>
-      </div>
+            <button
+              className={`heart bg-[#8C52FF] rounded-full p-5 ${
+                isSearchActivated ? "pulse morph-active" : "morph"
+              } relative z-20`}
+              onClick={() => setSearchActivated(true)}>
+              <AiFillHeart size={isSearchActivated ? 185 : 130} color="white" />
+            </button>
+          </div>
 
-      {!isSearchActivated ? (
-        <p className=" text-gray-400 text-sm mt-[-100px] mb-[100px]">
-          Tap Heart to start
-        </p>
+          {!isSearchActivated ? (
+            <p className=" text-gray-400 text-sm mt-[-100px] mb-[100px]">
+              Tap Heart to start
+            </p>
+          ) : (
+            <SearchingText />
+          )}
+        </div>
       ) : (
-        <SearchingText />
+        <Conversation setIsMatchFound={setIsMatchFound} />
       )}
     </div>
   );
