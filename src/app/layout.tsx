da8d40a1f1fc2c21d74b38f2ed30bf8fafa012b7
Shaun Niel Ochavo <shaunniel02@gmail.com>
2024-08-13 15:04:24 +0800

    Change name and remove file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Random Chat",
+  title: "Usap",
   description: "Chat with random people",
 };
 
